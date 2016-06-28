fa0d69119b09784fe43ef60d76afa89c79730420
JesusZorry <jesusemanuelzorrilla@gmail.com>
2016-06-28 20:32:14 -0300

    ajax

--- js/mine.js
+++ js/mine.js
@@ -7,3 +7,25 @@
               event.preventDefault();
               })
         });
+        $("#ArticOrt").on("click", function(){
+          getInformationByItem("../html/ArcOrt.html");
+          $(".activado").toggleClass("activado");
+          $("#ArticOrt").toggleClass("activado");
+        });
+
+        function getInformationByItem(item){
+          $.ajax({
+            method: "GET",
+            dataType: 'html',
+            url: item,
+            success: function(resultData){
+              //al decir que dataType es JSON, ya resultData es un objeto
+              var html = "";
+              html += resultData;
+              $("#ajaxContent").html(html);
+            },
+            error:function(jqxml, status, errorThrown){
+              console.log(errorThrown);
+            }
+          });
+        }
