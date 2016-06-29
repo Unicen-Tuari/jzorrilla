4923514f3727a202bf3b070390d763f3ed2319b7
JesusZorry <jesusemanuelzorrilla@gmail.com>
2016-06-29 11:26:51 -0300

    d4

--- js/comportamiento.js
+++ js/comportamiento.js
@@ -3,10 +3,16 @@ $(document).ready(function() {
   cargardato("../html/home.html");
   $("#articort").on("click",function() {cargardato("../html/ArcOrt.html")});
   $("#articmed").on("click",function () {cargardato("../html/ArcMed.html")});
-  $("#quienessomos").on("click", function() {cargardato ("../html/contac.html")});
+  $("#contacto").on("click", function() {cargardato ("../html/contac.html")});
   $("#holmes").on("click", function () {cargardato("../html/home.html")});
 });
-
+  $("dt").click(function(event){
+             var desplegable = $(this).next();
+             $('dd').not(desplegable).slideUp('fast');
+              desplegable.slideToggle('fast');
+              event.preventDefault();
+              })
+  });
   function cargardato(item){
     $.ajax({
         error:function () {
