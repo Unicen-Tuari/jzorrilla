be70c1520ac68fdb669e46bdb99f20ded6f5d77b
JesusZorry <jesusemanuelzorrilla@gmail.com>
2016-06-29 16:54:56 -0300

    ya merito

--- js/comportamiento.js
+++ js/comportamiento.js
@@ -83,26 +83,27 @@ function getInformationByGroup(){
     dataType: 'JSON',
     url: "http://web-unicen.herokuapp.com/api/group/" + grupo,
     success:function (resultData){
-     var lista = "";
-     for (var i = 0; i < resultData.information.length; i++) {
-       lista += "<ul>";
-       lista += "<li>nombre: " + resultData.information[i]["thing"].nombre + "</li>";
-       lista += "<li>dep: " + resultData.information[i]["thing"].descripcion + "</li>";
-       lista += "<li>precio: " + resultData.information[i]["thing"].precio + "</li>";
-       lista += "<li> <button class='btn btn-default borrar' type='button' > Borrar </button> </li>"
-       lista += "</ul>";
-       lista +="  <p id='bordefinal' > </p>"
-       $("#listadoproductos").html(lista);
-     }
+         var lista = "";
+         for (var i = 0; i < resultData.information.length; i++) {
+           lista += "<ul>";
+           lista += "<li>nombre: " + resultData.information[i]["thing"].nombre + "</li>";
+           lista += "<li>dep: " + resultData.information[i]["thing"].descripcion + "</li>";
+           lista += "<li>precio: " + resultData.information[i]["thing"].precio + "</li>";
+           lista += "<li> <button class='btn btn-default borrar' type='button' > Borrar </button> </li>"
+           lista += "</ul>";
+           lista +="  <p id='bordefinal' > </p>"
+           $("#listadoproductos").html(lista);
+         }
+       },
     error:function(jqxml,status,errorThrown){
       console.log(errorThrown);
     },
-      var botonesEliminar = $(".borrar");
-      for (var i = 0; i < botonesEliminar.length; i++) {
-        asignarEliminar(i, resultData.information[i]['_id']);
-      }
-    },
-});
+    var botonesEliminar = $(".borrar");
+    for (var i = 0; i < botonesEliminar.length; i++) {
+      asignarEliminar(i, resultData.information[i]['_id']);
+    }
+
+    });
 }
 
 function asignarEliminar(i, id){
