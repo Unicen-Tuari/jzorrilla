1ad1c37d2fd082f55e80de60e756f6affdcce45d
JesusZorry <jesusemanuelzorrilla@gmail.com>
2016-06-29 06:39:08 -0300

    f

--- js/mine.js
+++ js/mine.js
@@ -1,27 +1,26 @@
-"use strict";
-  $(document).ready(function(){
+
+$( document ).ready(function() {
   getInformationByItem("../html/home.html");
+  $("#holmes").on("click", function(){
+    getInformationByItem("../html/home.html");
+    $("#holmes").toggleClass("activado");
+  });
 
-    $("#holmes").on("click", function(){
-      getInformationByItem("../html/home.html");
-      $("#holmes").toggleClass("activado");
-      });
+  $("#ArticMed").on("click", function(){
+    getInformationByItem("../html/ArcMed.html");
+    $("#ArticMed").toggleClass("activado");
+  });
 
-    $("#ArticOrt").on("click", function(){
-      getInformationByItem("../html/ArcOrt.html");
-      $("#ArticOrt").toggleClass("activado");
-      });
-
-      $("#ArticMed").on("click", function(){
-        getInformationByItem("../html/ArcMed.html");
-        $("#ArticMed").toggleClass("activado");
-        });
-
-        $("#Contacto").on("click", function(){
-          getInformationByItem("../html/contac.html");
-          $("#Contacto").toggleClass("activado");
-          });
+  $("#ArticOrt").on("click", function(){
+    getInformationByItem("../html/ArcOrt.html");
+    $("#ArticOrt").toggleClass("activado");
+  });
+  $("#Contacto").on("click", function(){
+    getInformationByItem("../html/contac.html");
+    $("#Contacto").toggleClass("activado");
+  });
 });
+
 
  //              $(".Artic").hide();
 //            $("dt").click(function(event){
@@ -31,16 +30,19 @@
 //                    event.preventDefault();
 //                    })
 
-        function getInformationByItem(item){
-            $.ajax({
-            dataType: "HTML",
-            url: item,
-            success: function(receivedData){
-              var html = "";
-              html += receivedData;
-              $("#ajaxContent").html(html);
-              alert("cargo");
-              },
-                method: "GET"
-          });
-  }
+function getInformationByItem(item){
+  $.ajax({
+    method: "GET",
+    dataType: "HTML",
+    url: item,
+    success: function(resultData){
+      //al decir que dataType es JSON, ya resultData es un objeto
+      var html = "";
+      html += resultData;
+      $("#ajaxContent").html(html);
+    },
+    error:function(jqxml, status, errorThrown){
+      console.log(errorThrown);
+    }
+  });
+}
