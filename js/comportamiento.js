ec00695f9a85ff387e7f52dbc7a2d2fd8e08b39c
JesusZorry <jesusemanuelzorrilla@gmail.com>
2016-06-29 16:56:15 -0300

    deberia ya

--- js/comportamiento.js
+++ js/comportamiento.js
@@ -98,12 +98,11 @@ function getInformationByGroup(){
     error:function(jqxml,status,errorThrown){
       console.log(errorThrown);
     },
-    var botonesEliminar = $(".borrar");
-    for (var i = 0; i < botonesEliminar.length; i++) {
-      asignarEliminar(i, resultData.information[i]['_id']);
-    }
-
-    });
+  });
+  var botonesEliminar = $(".borrar");
+  for (var i = 0; i < botonesEliminar.length; i++) {
+    asignarEliminar(i, resultData.information[i]['_id']);
+  }
 }
 
 function asignarEliminar(i, id){
