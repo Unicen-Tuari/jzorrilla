a02c351602e6f3e5b8584e21e659af1df5b62caf
JesusZorry <jesusemanuelzorrilla@gmail.com>
2016-06-29 05:30:56 -0300

    asd
    asdasd

--- js/mine.js
+++ js/mine.js
@@ -4,30 +4,30 @@
       $(".activado").toggleClass("activado");
       $("#holmes").toggleClass("activado");
       });
-      	$(".Artic").hide();
-	$("dt").click(function(event){
-             var desplegable = $(this).next();
-             $('.Artic').not(desplegable).slideUp('fast');
-              desplegable.slideToggle('fast');
-              event.preventDefault();
-              })
+    $("#ArticOrt").on("click", function(){
+      getInformationByItem("../html/ArcOrt.html");
+      $(".activado").toggleClass("activado");
+      $("#ArticOrt").toggleClass("activado");
+      });
+      $("#ArticMed").on("click", function(){
+        getInformationByItem("../html/ArcMed.html");
+        $(".activado").toggleClass("activado");
+        $("#ArticMed").toggleClass("activado");
         });
-        $("#ArticOrt").on("click", function(){
-          getInformationByItem("../html/ArcOrt.html");
+        $("#Contacto").on("click", function(){
+          getInformationByItem("../html/contac.html");
           $(".activado").toggleClass("activado");
-          $("#ArticOrt").toggleClass("activado");
+          $("#Contacto").toggleClass("activado");
           });
-          $("#ArticMed").on("click", function(){
-            getInformationByItem("../html/ArcMed.html");
-            $(".activado").toggleClass("activado");
-            $("#ArticMed").toggleClass("activado");
-            });
-            $("#Contacto").on("click", function(){
-              getInformationByItem("../html/contac.html");
-              $(".activado").toggleClass("activado");
-              $("#Contacto").toggleClass("activado");
-              });
+        });
 
+              $(".Artic").hide();
+            $("dt").click(function(event){
+                   var desplegable = $(this).next();
+                   $('.Artic').not(desplegable).slideUp('fast');
+                    desplegable.slideToggle('fast');
+                    event.preventDefault();
+                    })
 
         function getInformationByItem(item){
             $.ajax({
