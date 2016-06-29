6d595ffabf0c2d72329663a4734c8d94257897b1
JesusZorry <jesusemanuelzorrilla@gmail.com>
2016-06-29 10:05:29 -0300

    d4

--- js/comportamiento.js
+++ js/comportamiento.js
@@ -1,25 +1,24 @@
 "use strict";
 $(document).ready(function() {
   Cargardato("../html/home.html");
+  $("#articort").on("click",function() {Cargardato("../html/arcort.html")});
+  $("#articmed").on("click",function () {Cargardato("../html/arcmed.html")});
+  $("#quienessomos").on("click", function() {Cargardato ("../html/contac.html")});
+  $("#holmes").on("click", function () {Cargardato("../html/home.html")});
 
   function MostrarDato(Dato){
     $("#ajaxContent").html(Dato);
   }
   function Cargardato(Data){
-    $.ajax(
-      {
+    $.ajax({
         type: "GET",
         url:Data,
+        dataType:'html',
         success: MostrarDato,
-        dataType:"html",
-      error:alert("no funka")
-      }
-    );
+        error:alert("no funka")
+      });
   }
-  $("#articort").on("click",function() {Cargardato("../html/arcort.html")});
-  $("#articmed").on("click",function () {Cargardato("../html/arcmed.html")});
-  $("#quienessomos").on("click", function() {Cargardato ("../html/contac.html")});
-  $("#holmes").on("click", function () {Cargardato("../html/home.html")});
+
 
 
 });
